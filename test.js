e44402102dad9376a9a38c107a7ff49e8e8cf9df
rdhss <ridhosuhendar1@gmail.com>
2024-08-04 23:18:34 +0700

    commit

--- test.js
+++
@@ -1,9 +0,0 @@
-function generateTransactionNumber() {
-    const firstPart = Math.floor(Math.random() * 900000) + 100000;
-    const secondPart = Math.floor(Math.random() * 9000) + 1000;
-    const transactionNumber = `${firstPart}-${secondPart}`;
-    return transactionNumber;
-}
-
-// Generate and print a transaction number
-console.log(generateTransactionNumber());
